28b8b12b106efc071a4e8f0d5b35fd4d5487ea28
natancabral <natancabral@gnnc.com.br>
2022-04-16 19:31:34 -0300

    up

--- README.md
+++ README.md
@@ -15,14 +15,33 @@ Reload express.js server. Kill process or port, reload server and, if you need, 
 - Kill process
 - Kill port
 - List pid processes
+- Other...
 
 `WARNING: Do not use the development server in a production environment.`
 
-## Install [<img src="https://github.com/natancabral/node-express-reload/blob/main/images/npm-tile.png">](https://www.npmjs.com/package/node-express-reload)
+## Install [<img src="https://github.com/natancabral/node-express-reload/blob/main/public/images/npm-tile.png">](https://www.npmjs.com/package/node-express-reload)
 
 ```shell
 npm install node-express-reload
 ```
+
+## Screens
+
+<p align="center">
+  <img src="https://github.com/natancabral/node-express-reload/blob/main/images/signin.png" alt="node-express-reload (Natan Cabral)"/>
+</p>
+<p align="center">
+  <img src="https://github.com/natancabral/node-express-reload/blob/main/images/help.png" alt="node-express-reload (Natan Cabral)"/>
+</p>
+<p align="center">
+  <img src="https://github.com/natancabral/node-express-reload/blob/main/images/reload.png" alt="node-express-reload (Natan Cabral)"/>
+</p>
+<p align="center">
+  <img src="https://github.com/natancabral/node-express-reload/blob/main/images/npm-ls.png" alt="node-express-reload (Natan Cabral)"/>
+</p>
+<p align="center">
+  <img src="https://github.com/natancabral/node-express-reload/blob/main/images/npm-ls.png" alt="node-express-reload (Natan Cabral)"/>
+</p>
 
 ## server.js
 
@@ -34,11 +53,14 @@ const PORT = 8080;
 // ** Secure Change **
 // ** change name /ner to /any-another-word **
 app.use('/ner', require("node-express-reload")({
-  key: '&HSN15KQi!Ç', // password
-  application: app, // app express()
+  username: 'admin', // optional (if not defined your username is admin)
+  password: '&HSN15KQi!Ç', // required
+  application: app, // application express
   serverfile: __filename, // ./index.js or ./server.js. call on restart
+  // pwcache: 7, // password cache in minutes
 }));
 
+// app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => res.send(`I'm pid ${process.pid} and port ${PORT}`));
 app.listen(PORT);
 ```
@@ -50,6 +72,11 @@ app.listen(PORT);
 > I'm pid 849113 and port 8080
 ```
 --> 
+
+## Access Terminal Virtual
+```
+GET http://localhost:8080/ner
+```
 
 ## Reload server
 ```
@@ -89,9 +116,12 @@ reload complete ✅
 | /**ner**/kill/:pid? | kill process | http://localhost:8080/ner/kill |
 | /**ner**/reload/:pid? | kill process and restart | http://localhost:8080/ner/reload |
 | /**ner**/list | pid list | http://localhost:8080/ner/list |
-| /**ner**/listall or /list-all | big pid list | http://localhost:8080/ner/listall|
+| /**ner**/listall or /list-all | big pid list | http://localhost:8080/ner/listall |
+| /**ner**/pid | show process id (PID) | http://localhost:8080/ner/pid |
 | /**ner**/npm/:type/:packages | Install and uninstall packages. type: i or u. | http://localhost:8080/ner/npm/i/pdfkit-table,cors |
 | /**ner**/npm/fix | npm fix | http://localhost:8080/ner/npm/fix |
+| /**ner**/npm/ls | npm ls | http://localhost:8080/ner/npm/ls |
+| /**ner**/npm/audit | npm audit | http://localhost:8080/ner/npm/audit |
 | /**ner**/npm/install | npm install package.json | http://localhost:8080/ner/npm/install |
 
 ## License
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const { send } = require("process");
-
 module.exports = function (settings) {
+
   const { exec } = require("child_process");
-  const express = require("express");
+  // Cookie
   const cookieParser = require("cookie-parser");
+  // Importing express module
+  const express = require("express");
+  // Importing express-session module
+  const session = require("express-session");
+  // Importing file-store module
+  const filestore = require("session-file-store")(session);
+  // Router
   const router = express.Router();
-
-  let { key, application, cache, production, serverfile } = settings;
-
-  // SET cookie
-  application.use(cookieParser());
+  
+  let { username, password, application, pwcache, production, serverfile, neruservar, storage } = settings;
 
   serverfile || (serverfile = "index.js");
-  cache || (cache = 1); // 1h
+  pwcache || (pwcache = 1); // 1h
+  neruservar || (neruservar = 'ner_user_auth');
+  storage || (storage = 'cookie'); // session
+  username || (username = 'admin');
 
   // // set
   // res.cookie(cookie_name, 'value', {
@@ -25,45 +31,167 @@ module.exports = function (settings) {
   // // redirect
   // res.redirect('/');
 
-  const GET_PID = `Im pid ${process.pid}`;
-  const SECURE_PROMPT_HTML = `
-  <script type="text/javascript">
-    var password = window.prompt("Enter KEY");
-    if(password === '${key}'){
-      document.write('👍 ${GET_PID}');
-      // window.location='/';
-    } else {
-      document.write('👎');      
-    } 
-  </script>
+  if(storage === 'cookie') {
+    // Use Cookie
+    application.use(
+      cookieParser()
+    );
+  } else {
+    // Use Session
+    application.use(
+      session({
+        name: "session-ner-name",
+        secret: "session-ner-key", // Secret key,
+        saveUninitialized: false,
+        resave: false,
+        // store: new filestore(),
+      })
+    );
+  }
+  
+  const SECURE_MESSAGE = `
+    Change your password. 
+    <li>uppercase</li> 
+    <li>lowercase</li> 
+    <li>number</li> 
+    <li>special character</li> 
+    <li>6 length</li> 
   `;
-  const SECURE_MESSAGE = `Change your password. <li>uppercase</li> <li>lowercase</li> <li>number</li> <li>special character</li> <li>6 length</li> `;
+
+  const SECURE_PROMPT_HTML = `
+    <script type="text/javascript">
+      var password = window.prompt("Enter KEY");
+      // send post value
+    </script>
+  `;
 
   // check pw strong
-  const checkPassWord = (p) =>
-    p.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/);
+  const checkPassWord = (p) => p.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/);
 
-  // welcome
-  router.get("/", (req, res) => {
-    console.log(req.cookies.pwkey);
-    res.send("👋 HI, Welcome node-express-reload.");
-  });
-
-  // init securety
-  router.get("/secure", (req, res) => {
-    console.log(req.cookies.pwkey);
-    res.cookie("pwkey", key, {
-      maxAge: 1000 * 60 * 60 * cache, // 1 hour
-    });
-
+  // Asking for the authorization
+  function authSimple(req, res, next) {
     if (checkPassWord(key)) {
       res.send(SECURE_PROMPT_HTML);
     } else {
       res.send(SECURE_MESSAGE);
     }
+  }
+
+  // Asking for the authorization
+  function auth(req, res, next) {
+    
+    if(!password) {
+      const err = 'Set password on node-express-load package.\nhttps://www.npmjs.com/package/node-express-reload';
+      res.send(`<pre>Error: ${err}`)
+      throw new Error(err);
+      return;
+    }
+
+    // console.log(
+    //   req.cookies,
+    //   req.signedCookies,
+    //   req.headers.authorization,
+    //   req.headers
+    // )
+
+    const getData = () => {
+      const authHeader = req.headers.authorization;
+      return authHeader ? new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':') : ['', ''];
+    }
+
+    const noAuthenticate = () => {
+      console.log('Unauthorized');
+      res.setHeader("WWW-Authenticate", "Basic")
+      res.sendStatus(401);
+    } 
+
+    let userSTORAGE;
+    
+    if(storage === 'cookie') {
+      userSTORAGE = req.cookies[neruservar];
+      // console.log('cookie', userSTORAGE);
+    } else {
+      userSTORAGE = req.session[neruservar];
+      // console.log('session', userSTORAGE);
+    }
+
+    // Checking for the authorization
+    if (userSTORAGE === username) {
+      // Allow
+      next();
+    } else if (!userSTORAGE) {
+      // Get headers authorization
+      const [usernameAUTH, passwordAUTH] = getData();
+      // Check user and pass
+      if (usernameAUTH === username && passwordAUTH === password) {
+        // Storage
+        // -----------------------------------------
+        if(storage === 'cookie') {
+          res.cookie(`${neruservar}`, username, {
+            path: '/ner',
+            // maxAge: 1000 * 60 * 60 * cache, // 1 hour
+            maxAge: 1000 * 60 * pwcache, // 1 min
+            // overwrite: true,
+            httpOnly: true,
+          });
+        } else {
+          // console.log('Entrou(2)');
+          req.session[neruservar] = username;
+        }
+        // -----------------------------------------
+        console.log('Authenticate');
+        next();
+      } else {
+        // Ops :(
+        return noAuthenticate();
+      }
+    } else {
+      // Ops :(
+      return noAuthenticate();
+    }
+  }
+    
+  // welcome
+  router.get("/", auth, (req, res) => {
+    // console.log(req.baseUrl);
+    // res.send("👋 HI, Welcome node-express-reload.");
+    res.sendFile( __dirname + '/src/terminal.html');
   });
 
-  router.get("/kill-port/:port", function (req, res) {
+  router.get("/pid", (req, res) => {
+    res.send(`PID ${process.pid}`);
+  });
+
+  router.get("/dirname", (req, res) => {
+    res.send(`PID ${__dirname}`);
+  });
+
+  router.get("/logout", (req, res) => {
+
+    res.setHeader("WWW-Authenticate", "Basic"); // realm="Access to the staging site", charset="UTF-8"
+    res.status(401);
+
+    if(storage === 'cookie') {
+      // Clear cookie
+      res.cookie(neruservar, '', { path: '/ner', maxAge: 1, expires: Date.now(0), overwrite: true, httpOnly: true });
+      res.clearCookie(neruservar);
+    } else {
+      // Clear session
+      req.session[neruservar] = null;
+      req.locals[neruservar] = null;
+      // req.session.destroy();  
+      // req.logout();
+    }
+
+    res.send("Logout. 👋 Clear all cookies and session.");
+  });
+
+  // init securety
+  router.get("/secure", authSimple, (req, res) => {
+    res.send("Secure - Simple Authentication.");
+  });
+
+  router.get("/kill-port/:port", auth, function (req, res) {
     // import exec method from child_process module
     const port = req.params.port >> 0;
     console.log(`Killing port ${port}`);
@@ -71,7 +199,7 @@ module.exports = function (settings) {
     return res.send(`Port ${port}`);
   });
 
-  router.get("/reload-port/:port", function (req, res) {
+  router.get("/reload-port/:port", auth, function (req, res) {
     // import exec method from child_process module
     const port = req.params.port >> 0;
     console.log(`reload port ${port}`);
@@ -79,7 +207,7 @@ module.exports = function (settings) {
     return res.send(`Reload ${port}`);
   });
 
-  router.get("/kill/:pid?", function (req, res) {
+  router.get("/kill/:pid?", auth, function (req, res) {
     // import exec method from child_process module
     const pid = req.params.pid || process.pid;
     console.log(`Killing pid ${pid}`);
@@ -94,7 +222,7 @@ module.exports = function (settings) {
   // Solution:
   // usar spawn
 
-  router.get("/reload/:pid?", function (req, res) {
+  router.get("/reload/:pid?", auth, function (req, res) {
     // import exec method from child_process module
     const pid = req.params.pid || process.pid;
     console.log(`Reload pid ${pid}`);
@@ -107,7 +235,7 @@ module.exports = function (settings) {
     return res.send(`Reload pid ${pid}`);
   });
 
-  router.get("/list", function (req, res) {
+  router.get("/list", auth, function (req, res) {
     let out = "";
     exec("ps -aef | grep 'node'", (e, stdout, stderr) => {
       if (e instanceof Error) {
@@ -167,10 +295,10 @@ module.exports = function (settings) {
     });
   }
 
-  router.get("/list-all", listall);
-  router.get("/listall", listall);
+  router.get("/list-all", auth, listall);
+  router.get("/listall", auth, listall);
 
-  router.get("/npm/:type/:list", function (req, res) {
+  router.get("/npm/:type/:list", auth, function (req, res) {
     const list = String(req.params.list)
       .replace(/\|/g, "/")
       .split(",")
@@ -205,7 +333,7 @@ module.exports = function (settings) {
     });
   });
 
-  router.get("/npm/fix", function (req, res) {
+  router.get("/npm/fix", auth, function (req, res) {
     let out = "";
     const scrpt = `npm audit fix`;
     exec(scrpt, (e, stdout, stderr) => {
@@ -223,7 +351,7 @@ module.exports = function (settings) {
     });
   });
 
-  router.get("/npm/install", function (req, res) {
+  router.get("/npm/install", auth, function (req, res) {
     let out = "";
     const scrpt = `npm install`;
     exec(scrpt, (e, stdout, stderr) => {
@@ -241,6 +369,41 @@ module.exports = function (settings) {
     });
   });
 
+  router.get("/npm/ls", auth, function (req, res) {
+    let out = "";
+    const scrpt = `npm ls`;
+    exec(scrpt, (e, stdout, stderr) => {
+      if (e instanceof Error) {
+        console.error(e);
+        //throw e;
+      }
+      console.log("stdout:\n", stdout);
+      console.log("stderr:\n", stderr);
+
+      out += stdout;
+      out += stderr;
+
+      res.send("<html><pre>" + out);
+    });
+  });
+
+  router.get("/npm/audit", auth, function (req, res) {
+    let out = "";
+    const scrpt = `npm audit`;
+    exec(scrpt, (e, stdout, stderr) => {
+      if (e instanceof Error) {
+        console.error(e);
+        //throw e;
+      }
+      console.log("stdout:\n", stdout);
+      console.log("stderr:\n", stderr);
+
+      out += stdout;
+      out += stderr;
+
+      res.send("<html><pre>" + out);
+    });
+  });
 
   return router;
 };
